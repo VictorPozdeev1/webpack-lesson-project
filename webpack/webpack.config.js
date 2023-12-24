37a6c4480bb8b5a68260a263d53ff9701354f12c
Victor Pozdeev <victorpozdeev@gmail.com>
2023-12-24 22:58:19 +0400

    Small fixes

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   mode: "development",
   plugins: [
-    newHtmlWebpackPlugin({
+    new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "..", "./src/index.html"),
     }),
   ],
