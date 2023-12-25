7888d30992b738007ce6a13cc2296d9e0a7682ea
Victor Pozdeev <victorpozdeev@gmail.com>
2023-12-26 01:28:36 +0400

    Multiple configs and envs

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ const App = (): JSX.Element => {
   return (
     <>
       <div>Init webpack-manual-setup project</div>
+      <p>Mode = {process.env.NODE_ENV}</p>
       <img height="300px" width="fitContent" src={image1} alt="" />
       <img height="200px" width="fitContent" src={image2} alt="" />
       <img height="100px" width="fitContent" src={ra} alt="" />
