f678e8755ef28701f08553a46845d1028cf79035
Victor Pozdeev <victorpozdeev@gmail.com>
2023-12-27 00:25:22 +0400

    Add ReactRefreshWebpackPlugin for dev mode

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,16 @@
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common.js");
 
-module.exports = ({ env }) =>
-  merge(commonConfig, require(`./webpack.${env}.js`));
+module.exports = ({ env }) => {
+  
+  // Не знаю, как тут через merge сделать, придётся так.
+  if (env === "dev") {
+    const babelLoaderNode = commonConfig.module.rules[0]?.use?.[0];
+    if (babelLoaderNode)
+      babelLoaderNode.options = {
+        plugins: [require.resolve("react-refresh/babel")],
+      };
+  }
+
+  return merge(commonConfig, require(`./webpack.${env}.js`));
+};
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,4 +1,7 @@
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
 module.exports = {
   mode: "development",
   devtool: "cheap-module-source-map",
+  plugins: [new ReactRefreshWebpackPlugin()],
 };
